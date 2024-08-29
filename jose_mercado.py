127905bc5b5704c9932c1e3eca246760a4ae5a59
carolbucker <melo.caroline@hotmail.com>
2024-08-28 21:42:22 -0300

    Incluindo os arquivos operadores aritimeticos e jose mercado

---
+++ jose_mercado.py
@@ -0,0 +1,11 @@
+saldo_inicial = 50.00
+custo_refrigerante = 8
+custo_pão = 4
+custo_mortadela = 39.90
+
+valorcompra = (custo_refrigerante * 2) + custo_pão + (custo_mortadela * 0.3)
+
+saldofinal = saldo_inicial - valorcompra
+print (f"Jose chegou com R${saldo_inicial} e saiu com R${saldofinal}")
+
+
---
+++ operadores_aritimeticos.py
@@ -0,0 +1,31 @@
+a = 5
+b = 3
+c = 4
+
+# Adição (+)
+resultado = a + b + c 
+print (f"Soma: {resultado}")
+
+# Subtração (-)
+resultado = c - b 
+print (f"Subtração: {resultado}")
+
+# Multiplicação (*)
+resultado = b * a 
+print (f"Multiplicação: {resultado}")
+
+# Divisão (/)
+resultado = a / b
+print (f"Divisão: {resultado}")
+
+#Divisão Inteira 
+resultado = a // b 
+print (f"Divisão Inteira: {resultado}")
+
+# Resto da Divisão (%)
+resultado = a % b
+print (f"Resto da Divisão: {resultado}")
+
+# Exponencial (**)
+resultado = a ** b
+print (f"Exponencial: {resultado}")
