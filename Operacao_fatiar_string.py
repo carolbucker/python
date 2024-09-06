e2675e99b8417171ea7ca259d52de9caee451c9a
carolbucker <melo.caroline@hotmail.com>
2024-09-05 21:54:33 -0300

    Operação fatiar

---
+++ Operacao_fatiar_string.py
@@ -0,0 +1,20 @@
+# Exemplo de string
+texto = "Ola, mundo!"
+
+# Fatiar do inicio até um índice específico
+print(texto[:5])
+
+# Fatiar a partir de um índice específico até o final
+print(texto[6:])
+
+#Fatiar do ínicio até o final com um passo específico
+print(texto[::2])
+
+# Fatiar com início e fim específicos
+print(texto[4:9])
+
+# Fatiar com um passo negativo para inverter uma substring
+print(texto[::-1])
+
+
+
