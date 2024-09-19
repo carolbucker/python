2942a006e878830f88e20dbbc6ece2797b9bbffa
carolbucker <melo.caroline@hotmail.com>
2024-09-18 21:45:10 -0300

    Arquivos da aula 18.09

---
+++ Desafio2/Desafio9.py
@@ -0,0 +1,54 @@
+# Calculadora Simples
+def calcular(op, numero1, numero2):
+    if op == "somar":
+        soma = numero1 + numero2
+        return soma
+    elif op == "subtrair":
+        subtracao = numero1 - numero2
+        return subtracao
+    elif op == "multiplicar":
+        multiplicacao = numero1 * numero2
+        return multiplicacao
+    elif op == "dividir":
+        divisao = numero1 / numero2
+        return divisao
+
+def main():
+    listaOperacoes = ["somar", "subtrair", "multiplicar", "dividir"]
+    numero1 = float(input("Digite o 1º número para calcular: "))
+    numero2 = float(input("Digite o 2º número para calcular: "))
+    op = input("Digite qual operacao quer realizar (somar, subtrair, multiplicar ou dividir): ")
+    
+    if op.lower() not in listaOperacoes:
+        print("Operação inválida")
+        novamente = input("Deseja tentar novamente? (S/N)").upper()
+        
+        if(novamente == "S"):
+            main()
+        else:
+            print("O programa está sendo finalizado!")
+        return
+
+    resultado = calcular(op, numero1, numero2)
+
+    if isinstance (resultado, str):
+        print(resultado)
+    else:
+        print(f"O resultado de {numero1} {op} {numero2} é {resultado} ")
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
